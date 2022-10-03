36d08af7b3fb578571b15b2e97b65afe21773c77
BRUVRY-LAGADEC <leo.bruvry.lagadec@ifremer.fr>
2022-10-03 11:09:21 +0200

    update token and finalize configuration

--- src/reducers/models/map.js
+++ src/reducers/models/map.js
@@ -26,5 +26,5 @@ export const layerModel = layerModelCore.mergeDeep({
         properties: {},
         data: undefined
     },
-    updateParameters: { time: false, bbox: false, filters: {} }
+    updateParameters: { time: true, bbox: false, filters: {} }
 });
